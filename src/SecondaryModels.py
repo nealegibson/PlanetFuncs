f8b81e299ac4167a9cab26b6b276f20d65416f6c
Neale Gibson <nealegibby@gmail.com>
2016-10-25 13:26:31 +0100

    added sec model

--- src/SecondaryModels.py
+++ src/SecondaryModels.py
@@ -53,3 +53,27 @@ def Sec_from_Transit_aRs_sq(par,t):
   return f
 
 #####################################################################
+
+def Sec_from_Transit_aRs_sq_norm(par,t):
+  """only returns norm..."""
+
+  T0,P,a_Rstar,p,b,foot,Tgrad,Tgrad2,depth = par
+  
+  #ensure b and p >= 0
+  #if b<0.: b=-b
+  #if p<0.: p=-p
+  
+  #calculate phase angle
+  #theta = (2*np.pi/P) * (t - T0)
+  
+  #normalised separation z
+  #z = np.sqrt( (a_Rstar*np.sin(theta))**2 + (b*np.cos(theta))**2 );
+
+  #calculate flux - use rescaled transit model with no limb darkening
+  #f = (FluxQuad(z,p,0.,0.) - 1.) / p**2. * depth + 1
+  #f *= (foot + (t - T0) * 24. * Tgrad) #time in hours for foot and Tgrad!
+  f = (foot + (t - T0) * 24. * Tgrad + ((t - T0) * 24.)**2 * Tgrad2)
+  #return flux
+  return f
+
+#####################################################################

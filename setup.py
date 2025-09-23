87137378f9bc59af9d294f9e36315a0077878dd8
Neale Gibson <n.gibson@tcd.ie>
2025-09-23 18:15:41 +0100

    minor update

--- setup.py
+++ setup.py
@@ -2,7 +2,24 @@
 from setuptools import setup, Extension
 import os
 import numpy as np
+import sys
 
+
+extensions = [
+    Extension("PlanetFuncs.TransitFlux.TransitFlux",sources=["src/TransitFlux/TransitFlux.c","src/TransitFlux/flux_quad.c","src/TransitFlux/flux_nonlin.c","src/TransitFlux/flux_func.c",
+      "src/TransitFlux/hyp2f1.c","src/TransitFlux/gamma.c","src/TransitFlux/psi.c","src/TransitFlux/round.c","src/TransitFlux/const.c","src/TransitFlux/fabs.c","src/TransitFlux/polevl.c","src/TransitFlux/mtherr.c"],libraries=['gsl',],
+      include_dirs=[np.get_include(),'src/TransitFlux/']
+      ),
+    Extension("PlanetFuncs.PlanetOrbit.PlanetOrbit",sources=["src/PlanetOrbit/PlanetOrbit.c","src/PlanetOrbit/PlanetOrbit_functions.c"]),
+    Extension("PlanetFuncs.RadialVelocity.RadialVelocity",sources=["src/RadialVelocity/RadialVelocity.c"]),
+    ]
+
+if sys.platform != 'win32':
+  extensions.append(
+      Extension("PlanetFuncs.TransitFlux.TransitFlux_ctypes",sources=["src/TransitFlux/TransitFlux_ctypes.c","src/TransitFlux/flux_quad.c","src/TransitFlux/flux_nonlin.c","src/TransitFlux/flux_func.c",
+        "src/TransitFlux/hyp2f1.c","src/TransitFlux/gamma.c","src/TransitFlux/psi.c","src/TransitFlux/round.c","src/TransitFlux/const.c","src/TransitFlux/fabs.c","src/TransitFlux/polevl.c","src/TransitFlux/mtherr.c"],libraries=['gsl','cblas','m'],)
+      )
+    
 setup(
   #metadata
   name = "PlanetFuncs", version = "1.0",
@@ -15,18 +32,7 @@ setup(
   package_dir={'PlanetFuncs':'src','PlanetFuncs.TransitFlux':'src/TransitFlux','PlanetFuncs.PlanetOrbit':'src/PlanetOrbit','PlanetFuncs.RadialVelocity':'src/RadialVelocity'},
   
   #define extension module(s)
-  ext_modules = [
-    Extension("PlanetFuncs.TransitFlux.TransitFlux",sources=["src/TransitFlux/TransitFlux.c","src/TransitFlux/flux_quad.c","src/TransitFlux/flux_nonlin.c","src/TransitFlux/flux_func.c",
-      "src/TransitFlux/hyp2f1.c","src/TransitFlux/gamma.c","src/TransitFlux/psi.c","src/TransitFlux/round.c","src/TransitFlux/const.c","src/TransitFlux/fabs.c","src/TransitFlux/polevl.c","src/TransitFlux/mtherr.c"],libraries=['gsl',],
-#      library_dirs=['./LibHyp/lib/'], #libhyp.a must be compiled first, and in a directory ./LibHyp/lib (obviously this can be changed if necessary)
-      include_dirs=[np.get_include(),'src/TransitFlux/']
-      ),
-    Extension("PlanetFuncs.PlanetOrbit.PlanetOrbit",sources=["src/PlanetOrbit/PlanetOrbit.c","src/PlanetOrbit/PlanetOrbit_functions.c"]),
-    Extension("PlanetFuncs.RadialVelocity.RadialVelocity",sources=["src/RadialVelocity/RadialVelocity.c"]),
-#     Extension("PlanetFuncs.TransitFlux.TransitFlux_ctypes",sources=["src/TransitFlux/TransitFlux_ctypes.c","src/TransitFlux/flux_quad.c","src/TransitFlux/flux_nonlin.c","src/TransitFlux/flux_func.c",
-#       "src/TransitFlux/hyp2f1.c","src/TransitFlux/gamma.c","src/TransitFlux/psi.c","src/TransitFlux/round.c","src/TransitFlux/const.c","src/TransitFlux/fabs.c","src/TransitFlux/polevl.c","src/TransitFlux/mtherr.c"],libraries=['gsl','cblas','m'],
-#       ),
-    ],
+  ext_modules = extensions,
   include_dirs=[np.get_include(),],      
   )
 

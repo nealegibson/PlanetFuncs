7d13d9d174743c844e8a768523b8ff24badcccfb
nealegibson <nealegibby@gmail.com>
2025-09-24 10:35:10 +0100

    updates to install files

--- setup.py
+++ setup.py
@@ -1,25 +1,31 @@
 
 from setuptools import setup, Extension
-import os
+import os, sys
 import numpy as np
-import sys
 
-
+#define extensions in advance
 extensions = [
     Extension("PlanetFuncs.TransitFlux.TransitFlux",sources=["src/TransitFlux/TransitFlux.c","src/TransitFlux/flux_quad.c","src/TransitFlux/flux_nonlin.c","src/TransitFlux/flux_func.c",
-      "src/TransitFlux/hyp2f1.c","src/TransitFlux/gamma.c","src/TransitFlux/psi.c","src/TransitFlux/round.c","src/TransitFlux/const.c","src/TransitFlux/fabs.c","src/TransitFlux/polevl.c","src/TransitFlux/mtherr.c"],libraries=['gsl',],
-      include_dirs=[np.get_include(),'src/TransitFlux/']
+      "src/TransitFlux/hyp2f1.c","src/TransitFlux/gamma.c","src/TransitFlux/psi.c","src/TransitFlux/round.c","src/TransitFlux/const.c","src/TransitFlux/fabs.c","src/TransitFlux/polevl.c","src/TransitFlux/mtherr.c"],
+      libraries=['gsl',],
+      include_dirs=[np.get_include(),'src/TransitFlux/',os.path.join(sys.prefix,'Library','include')],
+      library_dirs=[os.path.join(sys.prefix,'Library','lib'),os.path.join(sys.prefix,'lib')],
       ),
     Extension("PlanetFuncs.PlanetOrbit.PlanetOrbit",sources=["src/PlanetOrbit/PlanetOrbit.c","src/PlanetOrbit/PlanetOrbit_functions.c"]),
     Extension("PlanetFuncs.RadialVelocity.RadialVelocity",sources=["src/RadialVelocity/RadialVelocity.c"]),
     ]
 
+# add ctypes extension optionally
 if sys.platform != 'win32':
   extensions.append(
       Extension("PlanetFuncs.TransitFlux.TransitFlux_ctypes",sources=["src/TransitFlux/TransitFlux_ctypes.c","src/TransitFlux/flux_quad.c","src/TransitFlux/flux_nonlin.c","src/TransitFlux/flux_func.c",
-        "src/TransitFlux/hyp2f1.c","src/TransitFlux/gamma.c","src/TransitFlux/psi.c","src/TransitFlux/round.c","src/TransitFlux/const.c","src/TransitFlux/fabs.c","src/TransitFlux/polevl.c","src/TransitFlux/mtherr.c"],libraries=['gsl','cblas','m'],)
+        "src/TransitFlux/hyp2f1.c","src/TransitFlux/gamma.c","src/TransitFlux/psi.c","src/TransitFlux/round.c","src/TransitFlux/const.c","src/TransitFlux/fabs.c","src/TransitFlux/polevl.c","src/TransitFlux/mtherr.c"],
+        libraries=['gsl','cblas','m'],
+        include_dirs=[np.get_include(),os.path.join(sys.prefix,'Library','include')],
+        library_dirs=[os.path.join(sys.prefix,'Library','lib'),os.path.join(sys.prefix,'lib')],
+        )
       )
-    
+
 setup(
   #metadata
   name = "PlanetFuncs", version = "1.0",
@@ -33,6 +39,7 @@ setup(
   
   #define extension module(s)
   ext_modules = extensions,
-  include_dirs=[np.get_include(),],      
+  include_dirs=[np.get_include(),os.path.join(sys.prefix,'Library','include')],
+#  library_dirs=[os.path.join(sys.prefix,'Library','lib')],
   )
 
